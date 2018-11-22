38912187f86320570fd76c0c45a7de615c567505
Aceleus <cerbniakmgmail.com>
2018-11-22 15:43:45 +0100

    nic nie pomoże

---
+++ 531.py
@@ -0,0 +1,12 @@
+print("Wprowadź ciężar maksymalny")
+orm=int(input())
+
+print("Dzień 5 :\n pierwsza seria: 5x",orm*0.65 ,"\n Druga seria: 5x",orm*0.75,"\nTrzecia seria:+5x",orm*0.85)
+
+print("Dzień 3 :\n pierwsza seria: 3x",orm*0.70 ,"\n Druga seria: 3x",orm*0.80,"\nTrzecia seria: +3x",orm*0.90)
+
+print("Dzień 5,3,1 :\n pierwsza seria: 5x",orm*0.75 ,"\n Druga seria: 3x",orm*0.85,"\nTrzecia seria: +1x",orm*0.95)
+
+print("Deload :\n pierwsza seria:5x",orm*0.65 ,"\n Druga seria: 5x",orm*0.70,"\nTrzecia seria: 5x",orm*0.75)
+
+print("Ciężar ćwiczenia pomocniczego:\n jeśli 4x10:",orm*0.50,"\n jeśli 4x5:",orm*0.65)
